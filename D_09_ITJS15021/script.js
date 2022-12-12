562007d49cde635df7176c6d3d8d854b4f07047b
ITijana <tijana.milenkovic994@gmail.com>
2022-12-12 22:28:48 +0100

    domaci broj 9

---
+++ D_09_ITJS15021/script.js
@@ -0,0 +1,26 @@
+// 1. zadatak
+let n = 5;
+for (i = 1; i <= n; i++) {
+    if (i % 2 == 0) {
+        document.write(`<img class='parni' src="${1}.png">`);
+    }
+    else {
+        document.write(`<img class='neparni' src="${1}.png">`);
+    }
+}
+
+// 2. zadatak
+n = 11;
+broj = 2;
+
+for (i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+        broj++;
+    }
+}
+if (broj == 2) {
+    console.log(`Broj ${n} je prost broj`);
+}
+else {
+    console.log(`Broj ${n} nije prost broj`);
+}
